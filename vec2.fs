595917c0f4f02544340e1fb70f87072ed8f258e5
Lucas S. Vieira <lucasvieira@protonmail.com>
2021-08-08 13:54:52 -0300

    Add vec2 extras

--- vec2.fs
+++ vec2.fs
@@ -1,33 +1,48 @@
-c-function glm-vec2-copy     glm_vec2_copy     a a -- void
-c-function glm-vec2-zero     glm_vec2_zero     a -- void
-c-function glm-vec2-one      glm_vec2_one      a -- void
-c-function glm-vec2-dot      glm_vec2_dot      a a -- r
-c-function glm-vec2-cross    glm_vec2_cross    a a -- r
-c-function glm-vec2-norm2    glm_vec2_norm2    a -- r
-c-function glm-vec2-norm     glm_vec2_norm     a -- r
-c-function glm-vec2-add      glm_vec2_add      a a a -- void
-c-function glm-vec2-adds     glm_vec2_adds     a r a -- void
-c-function glm-vec2-sub      glm_vec2_sub      a a a -- void
-c-function glm-vec2-subs     glm_vec2_subs     a r a -- void
-c-function glm-vec2-mul      glm_vec2_mul      a a a -- void
-c-function glm-vec2-scale    glm_vec2_scale    a r a -- void
-c-function glm-vec2-scale-as glm_vec2_scale_as a r a -- void
-c-function glm-vec2-div      glm_vec2_div      a a a -- void
-c-function glm-vec2-divs     glm_vec2_divs     a r a -- void
-c-function glm-vec2-addadd   glm_vec2_addadd   a a a -- void
-c-function glm-vec2-subadd   glm_vec2_subadd   a a a -- void
-c-function glm-vec2-muladd   glm_vec2_muladd   a a a -- void
-c-function glm-vec2-muladds  glm_vec2_muladds  a r a -- void
-c-function glm-vec2-maxadd   glm_vec2_maxadd   a a a -- void
-c-function glm-vec2-minadd   glm_vec2_minadd   a a a -- void
-c-function glm-vec2-negate-to glm_vec2_negate_to a a -- void
-c-function glm-vec2-negate   glm_vec2_negate   a -- void
-c-function glm-vec2-normalize glm_vec2_normalize a -- void
-c-function glm-vec2-normalize-to glm_vec2_normalize_to a a -- void
-c-function glm-vec2-rotate   glm_vec2_rotate   a r a -- void
-c-function glm-vec2-distance2 glm_vec2_distance2 a a -- r
-c-function glm-vec2-distance glm_vec2_distance a a -- r
-c-function glm-vec2-maxv     glm_vec2_maxv     a a a -- void
-c-function glm-vec2-minv     glm_vec2_minv     a a a -- void
-c-function glm-vec2-clamp    glm_vec2_clamp    a r r -- void
-c-function glm-vec2-lerp     glm_vec2_lerp     a a r a -- void
+c-function glm-vec2-copy         glm_vec2_copy          a a -- void
+c-function glm-vec2-zero         glm_vec2_zero          a -- void
+c-function glm-vec2-one          glm_vec2_one           a -- void
+c-function glm-vec2-dot          glm_vec2_dot           a a -- r
+c-function glm-vec2-cross        glm_vec2_cross         a a -- r
+c-function glm-vec2-norm2        glm_vec2_norm2         a -- r
+c-function glm-vec2-norm         glm_vec2_norm          a -- r
+c-function glm-vec2-add          glm_vec2_add           a a a -- void
+c-function glm-vec2-adds         glm_vec2_adds          a r a -- void
+c-function glm-vec2-sub          glm_vec2_sub           a a a -- void
+c-function glm-vec2-subs         glm_vec2_subs          a r a -- void
+c-function glm-vec2-mul          glm_vec2_mul           a a a -- void
+c-function glm-vec2-scale        glm_vec2_scale         a r a -- void
+c-function glm-vec2-scale-as     glm_vec2_scale_as      a r a -- void
+c-function glm-vec2-div          glm_vec2_div           a a a -- void
+c-function glm-vec2-divs         glm_vec2_divs          a r a -- void
+c-function glm-vec2-addadd       glm_vec2_addadd        a a a -- void
+c-function glm-vec2-subadd       glm_vec2_subadd        a a a -- void
+c-function glm-vec2-muladd       glm_vec2_muladd        a a a -- void
+c-function glm-vec2-muladds      glm_vec2_muladds       a r a -- void
+c-function glm-vec2-maxadd       glm_vec2_maxadd        a a a -- void
+c-function glm-vec2-minadd       glm_vec2_minadd        a a a -- void
+c-function glm-vec2-negate-to    glm_vec2_negate_to     a a -- void
+c-function glm-vec2-negate       glm_vec2_negate        a -- void
+c-function glm-vec2-normalize    glm_vec2_normalize     a -- void
+c-function glm-vec2-normalize-to glm_vec2_normalize_to  a a -- void
+c-function glm-vec2-rotate       glm_vec2_rotate        a r a -- void
+c-function glm-vec2-distance2    glm_vec2_distance2     a a -- r
+c-function glm-vec2-distance     glm_vec2_distance      a a -- r
+c-function glm-vec2-maxv         glm_vec2_maxv          a a a -- void
+c-function glm-vec2-minv         glm_vec2_minv          a a a -- void
+c-function glm-vec2-clamp        glm_vec2_clamp         a r r -- void
+c-function glm-vec2-lerp         glm_vec2_lerp          a a r a -- void
+
+\ Extra
+c-function glm-vec2-fill         glm_vec2_fill          a r -- void
+c-function glm-vec2-eq           glm_vec2_eq            a r -- n
+c-function glm-vec2-eq-eps       glm_vec2_eq_eps        a r -- n
+c-function glm-vec2-eq-all       glm_vec2_eq_all        a -- n
+c-function glm-vec2-eqv          glm_vec2_eqv           a a -- n
+c-function glm-vec2-eqv-eps      glm_vec2_eqv_eps       a a -- n
+c-function glm-vec2-max          glm_vec2_max           a -- r
+c-function glm-vec2-min          glm_vec2_min           a -- r
+c-function glm-vec2-isnan        glm_vec2_isnan         a -- n
+c-function glm-vec2-isinf        glm_vec2_isinf         a -- n
+c-function glm-vec2-isvalid      glm_vec2_isvalid       a -- n
+c-function glm-vec2-sign         glm_vec2_sign          a a -- void
+c-function glm-vec2-sqrt         glm_vec2_sqrt          a a -- void
